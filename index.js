25ec986301e0ddd16e228d40dd745c3ce3a3c5ef
samdolbear <samuel@theracingmanager.com>
2023-12-11 18:20:24 +0000

    Make gen q + startgame functions

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const test = 2;
-console.log(test);
+function generateQuestion() {
+  const num1 = Math.floor(Math.random() * 10);
+  const num2 = Math.floor(Math.random() * 10);
+  const question = `${num1} + ${num2}`;
+  document.getElementById('question').textContent = question;
+  return num1 + num2; 
+}
+
+let correctAnswer;
+let score = 0;
+let highScore = 0;
+let timer;
+
+function startGame() {
+  score = 0;
+  document.getElementById('currentScore').textContent = score;
+  correctAnswer = generateQuestion();
+  timer = 10;
+}
+
